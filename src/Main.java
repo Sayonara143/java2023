55e8e89c7c50d0cf7f0ae34d6c6383284474bc6f
Alex Bykov <bikov23alex@gmail.com>
2023-03-06 11:31:17 +0500

    task 1.2

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,23 @@
+import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        System.out.print("Введите имя: ");
+        String name = in.next();
 
-        System.out.println("Hello world!");
+        System.out.println("Приветствую "+name);
+        in.close();
+
+        System.out.println("-------------------");
+        System.out.println("| градусы | sin() |");
+        System.out.println("-------------------");
+        for (int i = 0; i <= 45; i+=5){
+            double degrees = i;
+            double radians = Math.toRadians(degrees);
+            double sin =  Math.sin(radians);
+
+            System.out.println("|  " + degrees + "  | "+ String.format("%.4f", sin) + " |");
+            System.out.println("-------------------");
+        }
     }
 }
